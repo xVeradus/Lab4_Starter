554d8266cfcac10368e4ab6c73080685b1d8660c
Menko <menko@hornstein.me>
2025-04-29 21:42:32 -0700

    refactor: streamline sumValues and enhance input validation in discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,11 +7,8 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        let result = 0;
-
-        result = num1 + num2;
-
-        return result;
+        if (typeof num1 !== 'number' || typeof num2 !== 'number') return false;
+        return num1 + num2;
     }
     else {
         return false;
@@ -25,6 +22,9 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
+    if (!Array.isArray(prices) || typeof discount !== 'number' || discount < 0 || discount > 1) {
+        return false;
+    }
     const discounted = []
     const length = prices.length;
     if (prices.length === 0) {
