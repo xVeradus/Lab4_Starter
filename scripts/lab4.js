d4fddff669e483782a9e9e143b084e8fc4fb31eb
Menko <menko@hornstein.me>
2025-04-29 21:37:36 -0700

    refactor: improve variable declarations and return logic in sumValues and discountPrices functions

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -27,9 +27,11 @@ function sumValues(num1, num2, add) {
 function discountPrices(prices, discount) {
     const discounted = []
     const length = prices.length;
-    let discountedPrice = 0
+    if (prices.length === 0) {
+        return false;
+    }
     for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+        const discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
